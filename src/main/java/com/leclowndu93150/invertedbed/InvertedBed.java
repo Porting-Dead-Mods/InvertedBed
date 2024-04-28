a00caff6b612cd47fc58769114750928b85296e2
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-04-28 10:15:55 +0200

    Renaming

---
+++ src/main/java/com/leclowndu93150/invertedbed/InvertedBed.java
@@ -0,0 +1,52 @@
+package com.leclowndu93150.invertedbed;
+
+import net.minecraft.core.registries.Registries;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.MapColor;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.neoforge.registries.DeferredBlock;
+import net.neoforged.neoforge.registries.DeferredHolder;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+@Mod(InvertedBed.MODID)
+public class InvertedBed {
+    public static final String MODID = "invertedbed";
+
+    public static final DeferredRegister.Blocks BLOCKS = DeferredRegister.createBlocks(MODID);
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+    // Create a Deferred Register to hold CreativeModeTabs which will all be registered under the "examplemod" namespace
+    public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MODID);
+
+    // Creates a new Block with the id "examplemod:example_block", combining the namespace and path
+    public static final DeferredBlock<Block> INVERTED_BED = BLOCKS.registerSimpleBlock("inverted_bed", BlockBehaviour.Properties.of().mapColor(MapColor.STONE));
+    // Creates a new BlockItem with the id "examplemod:example_block", combining the namespace and path
+    public static final DeferredItem<BlockItem> INVERTED_BED_ITEM = ITEMS.registerSimpleBlockItem("inverted_bed", INVERTED_BED);
+
+    public static final DeferredHolder<CreativeModeTab, CreativeModeTab> INVERTED_TAB = CREATIVE_MODE_TABS.register("inverted_tab", () -> CreativeModeTab.builder()
+            .title(Component.translatable("itemGroup.invertedbed"))
+            .withTabsBefore(CreativeModeTabs.COMBAT)
+            .icon(() -> INVERTED_BED_ITEM.get().getDefaultInstance())
+            .displayItems((parameters, output) -> output.accept(INVERTED_BED_ITEM.get())).build());
+
+    public InvertedBed(IEventBus modEventBus) {
+
+        BLOCKS.register(modEventBus);
+
+        ITEMS.register(modEventBus);
+
+        CREATIVE_MODE_TABS.register(modEventBus);
+
+        NeoForge.EVENT_BUS.register(this);
+
+    }
+
+}
+
