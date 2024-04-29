718d667abbfd5041218fcb22300af854d9e02a2c
Leclowndu93150 <xdr.a123454321@gmail.com>
2024-04-30 00:26:38 +0200

    Imports

--- src/main/java/com/leclowndu93150/invertedbed/SleepingLogic.java
+++ src/main/java/com/leclowndu93150/invertedbed/SleepingLogic.java
@@ -1,17 +1,12 @@
 package com.leclowndu93150.invertedbed;
 
-import net.minecraft.client.multiplayer.ClientLevel;
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.LevelAccessor;
 import net.neoforged.bus.api.Event;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.Mod;
 import net.neoforged.neoforge.event.entity.player.PlayerWakeUpEvent;
 import net.neoforged.neoforge.event.entity.player.SleepingLocationCheckEvent;
 import net.neoforged.neoforge.event.entity.player.SleepingTimeCheckEvent;
-import net.neoforged.neoforge.event.level.SleepFinishedTimeEvent;
 
 import static com.leclowndu93150.invertedbed.Main.INVERTED_BED;
 import static com.leclowndu93150.invertedbed.Main.MODID;
@@ -30,7 +25,6 @@ public class SleepingLogic {
         if (event.getEntity().level().getBlockState(event.getSleepingLocation().get()).is(INVERTED_BED)) {
             event.setResult(event.getEntity().level().isDay() ? Event.Result.ALLOW : Event.Result.DENY);
         }
-        System.out.println("checking time: " + event.getSleepingLocation());
     }
 
     @SubscribeEvent
@@ -38,7 +32,6 @@ public class SleepingLogic {
         if (event.getEntity().level().getBlockState(event.getSleepingLocation()).getBlock().equals(INVERTED_BED)) {
             event.setResult(Event.Result.ALLOW);
         }
-        System.out.println("checking location: " + event.getSleepingLocation());
     }
 
 }
